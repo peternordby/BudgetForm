ff88266829711a1de5549dc86dbab61b90a6d8b2
Peter Skaar Nordby <petersn@stud.ntnu.no>
2024-01-03 16:13:44 +0100

    Add pwd to localstorage and update favicon

--- client/script.js
+++ client/script.js
@@ -2,29 +2,53 @@ const setToCurrentDate = () => {
     document.getElementById('date').valueAsDate = new Date();
 }
 
+const generateJSON = (form, password) => {
+    const data = new FormData(form)
+    const value = { ...Object.fromEntries(data.entries()), password: password }
+    const json = JSON.stringify(value)
+    return json
+}
+
+const setPassword = () => {
+    const password = prompt("Skriv inn passordet")
+    localStorage.setItem('password', password)
+}
+
+
 window.addEventListener('submit', async (event) => {
     event.preventDefault()
-    const form = event.target
-    const data = new FormData(form)
-    const value = Object.fromEntries(data.entries())
-    const json = JSON.stringify(value)
-    const url = form.action
-    console.log("json:", json)
-    console.log("url:", url)
 
-    const response = await fetch(url, {
-        method: 'POST',
-        body: json,
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    })
-    const result = await response
-    if (result.status === 200) {
-        alert("Det ble lagt til i regnskapet!")
+    // Retrieve password from local storage
+    let password = localStorage.getItem('password')
+
+    // If password is not set, ask for it
+    if (!password) {
+        setPassword()
     }
-    form.reset()
-    setToCurrentDate()
+
+    const json = generateJSON(event.target, password)
+    const url = 'https://server-wine-eta.vercel.app/addRow'
+
+    try {
+        const response = await fetch(url, {
+            method: 'POST',
+            body: json,
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        const result = await response
+        if (result.status === 200) {
+            alert("Det ble lagt til i regnskapet!")
+        }
+        form.reset()
+        setToCurrentDate()
+    }
+    catch (error) {
+        console.error('Error:', error)
+        localStorage.removeItem('password')
+        setPassword()
+    }
 })
 
 setToCurrentDate()
