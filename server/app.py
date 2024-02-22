50febb29c50dcc64005f57fab462b6d7a8c142c6
Peter Skaar Nordby <petersn@stud.ntnu.no>
2024-02-22 14:19:17 +0100

    Fix Google API quote limit

--- server/app.py
+++ server/app.py
@@ -8,17 +8,6 @@ app = Flask(__name__)
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
-scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
-creds = json.loads(os.environ.get('GOOGLE_CREDS'))
-with open('/tmp/gcreds.json', 'w') as f:
-    json.dump(creds, f)
-creds = ServiceAccountCredentials.from_json_keyfile_name(
-    '/tmp/gcreds.json', scope)
-client = gspread.authorize(creds)
-
-sheet = client.open("Regnskap")
-
-worksheet = sheet.worksheet("Utgifter 2024")
 
 @app.route('/')
 def index():
@@ -38,6 +27,18 @@ def add():
     password = data['password'] if 'password' in data else None
     if password != os.environ.get('PASSWORD'):
         return make_response("Unauthorized", 401)
+
+    scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
+    creds = json.loads(os.environ.get('GOOGLE_CREDS'))
+    with open('/tmp/gcreds.json', 'w') as f:
+        json.dump(creds, f)
+    creds = ServiceAccountCredentials.from_json_keyfile_name(
+        '/tmp/gcreds.json', scope)
+    client = gspread.authorize(creds)
+
+    sheet = client.open("Regnskap")
+
+    worksheet = sheet.worksheet("Utgifter 2024")
     
     new_row = [data['date'], data['date'], data['amount'], data['description'], data['category']]
     worksheet.append_row(new_row, value_input_option='USER_ENTERED')
